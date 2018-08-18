13789448446285b17fd10b8add5350732aaa4fcf
James Owens <jamesmarshallowens@gmail.com>
2018-08-18 16:29:46 -0400

    refactor mongoose

---
+++ server/models/user.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+const User = mongoose.model('User', {
+  email: { type: 'String', trim: true, minlength: 1, required: true },
+});
+
+module.exports = { User };
