73c0ed7587e84a06e1c4ca182e8fddc4641228fb
James Owens <jamesmarshallowens@gmail.com>
2018-08-18 17:59:36 -0400

    heroku

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+var { ObjectID } = require('mongodb');
 
 const { mongoose } = require('./db/mongoose');
 const { User } = require('./models/user');
@@ -7,6 +8,7 @@ const { Todo } = require('./models/todo');
 
 const idPrint = id => console.log(id);
 const app = express();
+const process = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
@@ -20,8 +22,28 @@ app.get('/todos', (req, res) => {
   Todo.find().then(todos => res.send({ todos }), e => res.status(400).send(e));
 });
 
-app.listen(3000, () => {
-  console.log('started on port 3000');
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id)
+    .then(todo => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+
+      res.send({ todo });
+    })
+    .catch(e => {
+      res.status(400).send();
+    });
+});
+
+app.listen(port, () => {
+  console.log(`started on ${port}`);
 });
 
 module.exports = { app };
